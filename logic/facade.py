6abfb30e6d8b4b03b16f1b705e339c20133cd074
Minish144 <varlamow.col@yahoo.com>
2020-07-25 17:23:29 +0300

    getting random songs

--- logic/facade.py
+++ logic/facade.py
@@ -5,8 +5,10 @@ import pandas as pd
 
 class Facade():
     def __init__(self):
-        self.themes = AnimeThemes().get_openings_list()
-        self.moe = OpeningsMoe().get_openings_list()
+        try:
+            self.song_set = AnimeThemes().get_openings_list()
+        except:
+            self.song_set = OpeningsMoe().get_openings_list()
 
     def __op_type(self, op_type: int) -> str:
         if op_type == 1:
@@ -18,12 +20,12 @@ class Facade():
 
     def __op_type_handle(self, df: pd.DataFrame, op_type: int) -> pd:
         contains = self.__op_type(op_type)
-        return df[~df.C.str.contains(contains)]
+        return df[df['Song_Type'].str.contains(contains)]
 
     def get_random_webms(self, count: int, op_type: int = 3):
         if op_type in [1, 2]:
-            df = self.__op_type_handle(self.themes, op_type)
+            df = self.__op_type_handle(self.song_set, op_type)
         else:
-            df = self.themes
+            df = self.song_set
         response = df.sample(n=count, replace=False, random_state=1)
         return response
