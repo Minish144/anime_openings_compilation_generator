bc7a26eba53a067a7857fecba6eba06f1f364a25
Minish144 <varlamow.col@yahoo.com>
2020-07-27 16:47:34 +0300

    utils update

--- utils/utils.py
+++ utils/utils.py
@@ -1,34 +1,43 @@
 import pandas as pd
 from fuzzywuzzy import fuzz
 import string
-
+import re
 class Utils():
     def __init__(self):
         pass
 
-    def find_simillar(self, lst: list, req: str):
-        response_list = []
-
-        for elem in lst:
-            if req in elem:
-                response_list.append(elem)
-            elif fuzz.ratio(elem, req) > 60:
-                response_list.append(elem)
-
-    def get_unique_names(self, df: pd.DataFrame, column: str) -> list:
-        return df[column].unique()
-
     def __clear_string_from_garbage(self, strr: str) -> str:
         for char in string.punctuation:
             strr = strr.replace(char,"")
+            
+        strr = re.sub(r'\s+', ' ', strr)
 
         return strr
 
-    def clear_list_of_strings_from_garbage(self, strings: list) -> list:
+    def __clear_list_of_strings_from_garbage(self, strings: list) -> list:
         response = []
 
         for strr in strings:
             response.append(self.__clear_string_from_garbage(strr))
         
         return response
+
+    def find_simillar(self, lst: list, req: str):
+        response_list = []
+
+        req = self.__clear_string_from_garbage(req)
+        lst = self.__clear_list_of_strings_from_garbage(lst)
+
+        print(lst)
+
+        for elem in lst:
+            if req in elem:
+                response_list.append(elem)
+            elif fuzz.ratio(elem, req) > 60:
+                response_list.append(elem)
+        
+        return response_list
+
+    def get_unique_names(self, df: pd.DataFrame, column: str) -> list:
+        return df[column].unique()
         
